9909cdf871a98f01257e2db25bab464b6b1da361
Kriszta <krisztavajda@gmail.com>
2018-01-14 14:45:50 +0100

    Update and complete MastHead

--- src/components/MastHead/MastHead.js
+++ src/components/MastHead/MastHead.js
@@ -8,9 +8,11 @@ class MastHead extends Component {
 	render() {
 		return (
 				<header className="masthead">
-					<div className="masthead__content">
-						<h1 className="masthead__title">Thank you for using ColourBox!</h1>
-						<p className="masthead__lead">You are now signed out, but your journey doesn't have to end here.</p>
+					<div className="masthead__foreground u-container">
+						<div className="masthead__content">
+							<h1 className="masthead__title">Thank you for using ColourBox!</h1>
+							<p className="masthead__lead">You are now signed out, but your journey doesn't have to end here.</p>
+						</div>
 					</div>
 					<img className="masthead__image" src={hero} alt="girl smelling flowers"/>
 				</header>
